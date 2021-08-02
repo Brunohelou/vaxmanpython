4d22d74965d84ac33d359248c84d332ff9f68849
Brunohelou <brunorhelou@gmail.com>
2021-08-02 15:07:10 -0300

    vaxman

---
+++ vaxman.py
@@ -0,0 +1,614 @@
+#Pacman in Python with PyGame
+
+#Created by: https://github.com/hbokmann/Pacman
+#Edited by: https://github.com/Brunohelou
+# repository of the project : 
+# Project for EA Software Engineering Virtual Experience Program
+ 
+
+
+from hashlib import new
+import pygame
+import time
+from random import randint, random
+black = (0,0,0)
+white = (255,255,255)
+blue = (0,0,255)
+green = (0,255,0)
+red = (255,0,0)
+purple = (255,0,255)
+yellow   = ( 255, 255,   0)
+
+# set how long in seconds till a virus multiply since it's been born
+time_to_multiply = 5
+
+Trollicon=pygame.image.load('./images/Trollman.png')
+pygame.display.set_icon(Trollicon)
+
+#Add music
+pygame.mixer.init()
+
+
+# This class represents the bar at the bottom that the player controls
+class Wall(pygame.sprite.Sprite):
+    # Constructor function
+    def __init__(self,x,y,width,height, color):
+        # Call the parent's constructor
+        pygame.sprite.Sprite.__init__(self)
+  
+        # Make a blue wall, of the size specified in the parameters
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+  
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.top = y
+        self.rect.left = x
+
+#list of sprites
+sprites = ["corona_v.png","corona.png"]
+
+
+# This creates all the walls in room 1
+def setupRoomOne(all_sprites_list):
+    # Make the walls. (x_pos, y_pos, width, height)
+    wall_list=pygame.sprite.RenderPlain()
+    # This is a list of walls. Each is in the form [x, y, width, height]
+    walls = [ [0,0,6,600],
+              [0,0,600,6],
+              [0,600,606,6],
+              [600,0,6,606],
+              [300,0,6,66],
+              [60,60,186,6],
+              [360,60,186,6],
+              [60,120,66,6],
+              [60,120,6,126],
+              [180,120,246,6],
+              [300,120,6,66],
+              [480,120,66,6],
+              [540,120,6,126],
+              [120,180,126,6],
+              [120,180,6,126],
+              [360,180,126,6],
+              [480,180,6,126],
+              [180,240,6,126],
+              [180,360,246,6],
+              [420,240,6,126],
+              [240,240,42,6],
+              [324,240,42,6],
+              [240,240,6,66],
+              [240,300,126,6],
+              [360,240,6,66],
+              [0,300,66,6],
+              [540,300,66,6],
+              [60,360,66,6],
+              [60,360,6,186],
+              [480,360,66,6],
+              [540,360,6,186],
+              [120,420,366,6],
+              [120,420,6,66],
+              [480,420,6,66],
+              [180,480,246,6],
+              [300,480,6,66],
+              [120,540,126,6],
+              [360,540,126,6]
+            ]
+     
+    # Loop through the list. Create the wall, add it to the list
+    for item in walls:
+        wall=Wall(item[0],item[1],item[2],item[3],white)
+        wall_list.add(wall)
+        all_sprites_list.add(wall)
+         
+    # return our new list
+    return wall_list
+
+def setupGate(all_sprites_list):
+      gate = pygame.sprite.RenderPlain()
+      gate.add(Wall(282,242,42,2,white))
+      all_sprites_list.add(gate)
+      return gate
+
+# This class represents the ball        
+# It derives from the "Sprite" class in Pygame
+class Block(pygame.sprite.Sprite):
+     
+    # Constructor. Pass in the color of the block, 
+    # and its x and y position
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self) 
+ 
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = pygame.Surface([width, height])
+        self.image.fill(white)
+        self.image.set_colorkey(white)
+        pygame.draw.ellipse(self.image,color,[0,0,width,height])
+ 
+        # Fetch the rectangle object that has the dimensions of the image
+        # image.
+        # Update the position of this object by setting the values 
+        # of rect.x and rect.y
+        self.rect = self.image.get_rect() 
+
+# This class represents the bar at the bottom that the player controls
+class Player(pygame.sprite.Sprite):
+  
+    # Set speed vector
+    init_x = 0
+    init_y = 0
+    change_x=0
+    change_y=0
+  
+    # Constructor function
+    def __init__(self,x,y, filename):
+        self.init_x = x
+        self.init_y = y
+        # Call the parent's constructor
+        pygame.sprite.Sprite.__init__(self)
+   
+        # Set height, width
+        self.image = pygame.image.load(filename).convert()
+  
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.top = y
+        self.rect.left = x
+        self.prev_x = x
+        self.prev_y = y
+
+    # Clear the speed of the player
+    def prevdirection(self):
+        self.prev_x = self.change_x
+        self.prev_y = self.change_y
+
+    # Change the speed of the player
+    def changespeed(self,x,y):
+        self.change_x+=x
+        self.change_y+=y
+          
+    # Find a new position for the player
+    def update(self,walls,gate):
+        # Get the old position, in case we need to go back to it
+        
+        old_x=self.rect.left
+        new_x=old_x+self.change_x
+        prev_x=old_x+self.prev_x
+        self.rect.left = new_x
+        
+        old_y=self.rect.top
+        new_y=old_y+self.change_y
+        prev_y=old_y+self.prev_y
+
+        # Did this update cause us to hit a wall?
+        x_collide = pygame.sprite.spritecollide(self, walls, False)
+        if x_collide:
+            # Whoops, hit a wall. Go back to the old position
+            self.rect.left=old_x
+            # self.rect.top=prev_y
+            # y_collide = pygame.sprite.spritecollide(self, walls, False)
+            # if y_collide:
+            #     # Whoops, hit a wall. Go back to the old position
+            #     self.rect.top=old_y
+            #     print('a')
+        else:
+
+            self.rect.top = new_y
+
+            # Did this update cause us to hit a wall?
+            y_collide = pygame.sprite.spritecollide(self, walls, False)
+            if y_collide:
+                # Whoops, hit a wall. Go back to the old position
+                self.rect.top=old_y
+                # self.rect.left=prev_x
+                # x_collide = pygame.sprite.spritecollide(self, walls, False)
+                # if x_collide:
+                #     # Whoops, hit a wall. Go back to the old position
+                #     self.rect.left=old_x
+                #     print('b')
+
+        if gate != False:
+          gate_hit = pygame.sprite.spritecollide(self, gate, False)
+          if gate_hit:
+            self.rect.left=old_x
+            self.rect.top=old_y
+
+#Inheritime Player klassist
+class Virus(Player):
+    p_turn = 0
+    p_steps = 0
+    virus_directions = []
+    time = time.time()
+
+    def __init__(self,x,y, filename, directions):
+      self.virus_directions = directions
+      self.time = time.time()
+      super().__init__(x,y, filename)
+      
+
+
+    def multiply(self):
+      #every 30 seconds create a new virus from same genetic code, but with collor change(mutation)
+
+      if time.time() - self.time > time_to_multiply:
+        self.time = time.time()
+        a = Virus( self.init_x, self.init_y, "images/"+sprites[randint(0,len(sprites)-1)], self.virus_directions)
+    
+    
+        return a
+      return 0
+
+
+    # Change the speed of the virus
+    def changespeed(self,turn,steps):
+      self.p_turn = turn
+      self.p_steps = steps
+      try:
+        z=self.virus_directions[turn][2]
+        
+        if steps < z:
+          self.change_x=self.virus_directions[turn][0]
+          self.change_y=self.virus_directions[turn][1]
+          steps+=1
+        else:
+          if turn < len(self.virus_directions):
+            turn+=1
+          else:
+            turn = 0
+          self.change_x=self.virus_directions[turn][0]
+          self.change_y=self.virus_directions[turn][1]
+          steps = 0
+        return [turn,steps]
+      except IndexError:
+         return [0,0]
+
+Pinky_directions = [
+[0,-30,4],
+[15,0,9],
+[0,15,11],
+[-15,0,23],
+[0,15,7],
+[15,0,3],
+[0,-15,3],
+[15,0,19],
+[0,15,3],
+[15,0,3],
+[0,15,3],
+[15,0,3],
+[0,-15,15],
+[-15,0,7],
+[0,15,3],
+[-15,0,19],
+[0,-15,11],
+[15,0,9]
+]
+
+Blinky_directions = [
+[0,-15,4],
+[15,0,9],
+[0,15,11],
+[15,0,3],
+[0,15,7],
+[-15,0,11],
+[0,15,3],
+[15,0,15],
+[0,-15,15],
+[15,0,3],
+[0,-15,11],
+[-15,0,3],
+[0,-15,11],
+[-15,0,3],
+[0,-15,3],
+[-15,0,7],
+[0,-15,3],
+[15,0,15],
+[0,15,15],
+[-15,0,3],
+[0,15,3],
+[-15,0,3],
+[0,-15,7],
+[-15,0,3],
+[0,15,7],
+[-15,0,11],
+[0,-15,7],
+[15,0,5]
+]
+
+Inky_directions = [
+[30,0,2],
+[0,-15,4],
+[15,0,10],
+[0,15,7],
+[15,0,3],
+[0,-15,3],
+[15,0,3],
+[0,-15,15],
+[-15,0,15],
+[0,15,3],
+[15,0,15],
+[0,15,11],
+[-15,0,3],
+[0,-15,7],
+[-15,0,11],
+[0,15,3],
+[-15,0,11],
+[0,15,7],
+[-15,0,3],
+[0,-15,3],
+[-15,0,3],
+[0,-15,15],
+[15,0,15],
+[0,15,3],
+[-15,0,15],
+[0,15,11],
+[15,0,3],
+[0,-15,11],
+[15,0,11],
+[0,15,3],
+[15,0,1],
+]
+
+Clyde_directions = [
+[-30,0,2],
+[0,-15,4],
+[15,0,5],
+[0,15,7],
+[-15,0,11],
+[0,-15,7],
+[-15,0,3],
+[0,15,7],
+[-15,0,7],
+[0,15,15],
+[15,0,15],
+[0,-15,3],
+[-15,0,11],
+[0,-15,7],
+[15,0,3],
+[0,-15,11],
+[15,0,9],
+]
+
+
+
+# Call this function so the Pygame library can initialize itself
+pygame.init()
+  
+
+# Create an 606x606 sized screen
+screen = pygame.display.set_mode([606, 606])
+
+# This is a list of 'sprites.' Each block in the program is
+# added to this list. The list is managed by a class called 'RenderPlain.'
+
+
+# Set the title of the window
+pygame.display.set_caption('Vaxman')
+
+# Create a surface we can draw on
+background = pygame.Surface(screen.get_size())
+
+# Used for converting color maps and such
+background = background.convert()
+  
+# Fill the screen with a black background
+background.fill(black)
+
+
+
+clock = pygame.time.Clock()
+
+pygame.font.init()
+font = pygame.font.Font("freesansbold.ttf", 24)
+
+#default locations for Pacman and viruss
+w = 303-16 #Width
+p_h = (7*60)+19 #Pacman height
+m_h = (4*60)+19 #Monster height
+b_h = (3*60)+19 #Binky height
+i_w = 303-16-32 #Inky width
+c_w = 303+(32-16) #Clyde width
+
+def startGame():
+
+  score = 0
+
+  all_sprites_list = pygame.sprite.RenderPlain()
+
+  block_list = pygame.sprite.RenderPlain()
+
+  virus_list = pygame.sprite.RenderPlain()
+
+  pacman_collide = pygame.sprite.RenderPlain()
+
+  wall_list = setupRoomOne(all_sprites_list)
+
+  gate = setupGate(all_sprites_list)
+
+
+
+
+  # Create the player paddle object and starter viruses
+  Pacman = Player( w, p_h, "images/Trollman.png" )
+  all_sprites_list.add(Pacman)
+  pacman_collide.add(Pacman)
+   
+
+  
+
+  i = 0
+  #amount of initial virus. With range 4 you have top variety of directions (inky, pinky, clyde, blinky)
+  #adding range 5 for example will create 5 viruses, but there will be two Pinky walking together.
+  for i in range(4):
+    if i%4 == 0:
+      a = Virus( w, m_h, "images/"+sprites[randint(0,len(sprites)-1)], Pinky_directions)
+      virus_list.add(a)
+      all_sprites_list.add(a)
+    if i%4 == 1:
+      a = Virus( c_w, m_h, "images/"+sprites[randint(0,len(sprites)-1)], Clyde_directions)
+      virus_list.add(a)
+      all_sprites_list.add(a)
+    if i%4 == 2:
+        a=Virus( i_w, m_h, "images/"+sprites[randint(0,len(sprites)-1)], Inky_directions )
+        virus_list.add(a)
+        all_sprites_list.add(a)
+    if i%4 == 3:
+        a=Virus( w, b_h, "images/"+sprites[randint(0,len(sprites)-1)], Blinky_directions )
+        virus_list.add(a)
+        all_sprites_list.add(a)
+
+
+  
+
+  # Draw the grid
+  for row in range(19):
+      for column in range(19):
+          if (row == 7 or row == 8) and (column == 8 or column == 9 or column == 10):
+              continue
+          else:
+            block = Block(yellow, 4, 4)
+
+            # Set a random location for the block
+            block.rect.x = (30*column+6)+26
+            block.rect.y = (30*row+6)+26
+
+            b_collide = pygame.sprite.spritecollide(block, wall_list, False)
+            p_collide = pygame.sprite.spritecollide(block, pacman_collide, False)
+            if b_collide:
+              continue
+            elif p_collide:
+              continue
+            else:
+              # Add the block to the list of objects
+              block_list.add(block)
+              all_sprites_list.add(block)
+
+  done = False
+  
+  bll = len(block_list)
+
+  while done == False:
+      # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              done=True
+
+          if event.type == pygame.KEYDOWN:
+              if event.key == pygame.K_LEFT:
+                  Pacman.changespeed(-30,0)
+              if event.key == pygame.K_RIGHT:
+                  Pacman.changespeed(30,0)
+              if event.key == pygame.K_UP:
+                  Pacman.changespeed(0,-30)
+              if event.key == pygame.K_DOWN:
+                  Pacman.changespeed(0,30)
+
+          if event.type == pygame.KEYUP:
+              if event.key == pygame.K_LEFT:
+                  Pacman.changespeed(30,0)
+              if event.key == pygame.K_RIGHT:
+                  Pacman.changespeed(-30,0)
+              if event.key == pygame.K_UP:
+                  Pacman.changespeed(0,30)
+              if event.key == pygame.K_DOWN:
+                  Pacman.changespeed(0,-30)
+          
+      # ALL EVENT PROCESSING SHOULD GO ABOVE THIS COMMENT
+   
+      # ALL GAME LOGIC SHOULD GO BELOW THIS COMMENT
+      Pacman.update(wall_list,gate)
+
+      def virus_routine(virus):
+          returned = virus.changespeed(virus.p_turn,virus.p_steps)
+          virus.p_turn = returned[0]
+          virus.p_steps = returned[1]
+          virus.changespeed(virus.p_turn,virus.p_steps)
+          virus.update(wall_list,False)
+
+      #routine to dinamicaly update virus position
+      for virus in virus_list:
+        virus_routine(virus)
+        new_virus = virus.multiply()
+        if new_virus != 0:
+          virus_list.add(new_virus)
+          all_sprites_list.add(new_virus)
+          for i in range(randint(0, 200)): #loop no initialize the new virus in a random new position
+            virus_routine(new_virus)
+
+      # If pacman colides with any object inside virus list, virus is killed from the list and not rendered
+      pygame.sprite.spritecollide(Pacman, virus_list, True)
+
+      # See if the Pacman block has collided with anything.
+      blocks_hit_list = pygame.sprite.spritecollide(Pacman, block_list, True)
+       
+      # Check the list of collisions.
+      if len(blocks_hit_list) > 0:
+        score += len(blocks_hit_list)
+      
+      # ALL GAME LOGIC SHOULD GO ABOVE THIS COMMENT
+   
+      # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
+      screen.fill(black)
+        
+      wall_list.draw(screen)
+      gate.draw(screen)
+      all_sprites_list.draw(screen)
+      virus_list.draw(screen)
+
+      text=font.render("Score: "+str(score)+"/"+str(bll), True, yellow)
+      screen.blit(text, [10, 10])
+      text=font.render("Virus: "+str(len(virus_list)), True, yellow)
+      screen.blit(text, [450, 10])
+
+      if score  >= bll:
+        doNext("Congratulations, you won!",145,all_sprites_list,block_list,virus_list,pacman_collide,wall_list,gate) 
+
+      if len(virus_list) >= 32 :
+        doNext("Game Over",235,all_sprites_list,block_list,virus_list,pacman_collide,wall_list,gate)
+
+      # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
+      
+      pygame.display.flip()
+    
+      clock.tick(10)
+
+def doNext(message,left,all_sprites_list,block_list,virus_list,pacman_collide,wall_list,gate):
+  while True:
+      # ALL EVENT PROCESSING SHOULD GO BELOW THIS COMMENT
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_ESCAPE:
+            pygame.quit()
+          if event.key == pygame.K_RETURN:
+            del all_sprites_list
+            del block_list
+            del virus_list
+            del pacman_collide
+            del wall_list
+            del gate
+            startGame()
+
+      #Grey background
+      w = pygame.Surface((400,200))  # the size of your rect
+      w.set_alpha(10)                # alpha level
+      w.fill((128,128,128))           # this fills the entire surface
+      screen.blit(w, (100,200))    # (0,0) are the top-left coordinates
+
+      #Won or lost
+      text1=font.render(message, True, white)
+      screen.blit(text1, [left, 233])
+
+      text2=font.render("To play again, press ENTER.", True, white)
+      screen.blit(text2, [135, 303])
+      text3=font.render("To quit, press ESCAPE.", True, white)
+      screen.blit(text3, [165, 333])
+
+      pygame.display.flip()
+
+      clock.tick(10)
+
+startGame()
+
+pygame.quit()
